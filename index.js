b6fb9468e0dfe8aaf8eb3f328ee429bde41a1d82
danmedani <danmedani@gmail.com>
2015-04-11 03:34:05 -0700

    + couch db

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+
+	var nano = require('nano')('http://localhost:5984');
+	nano.db.create('books');
+	var books = nano.db.use('books');
+
+	//Insert a book document in the books database
+	books.insert({name: 'The Art of war'}, null, function(err, body) {
+	  if (!err){
+	    console.log(body);
+	  }
+	});
+
+	//Get a list of all books
+	books.list(function(err, body){
+	  console.log(body.rows);
+	}
+
+	
   response.send('Hello World!')
 })
 
