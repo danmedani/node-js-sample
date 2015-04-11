5ef811f2600c12c42b81ed579200cfa7d57cc0ef
danmedani <danmedani@gmail.com>
2015-04-11 03:45:05 -0700

    k

--- index.js
+++ index.js
@@ -6,24 +6,22 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
 
-	var nano = require('nano')('http://localhost:5984');
-	nano.db.create('books');
-	var books = nano.db.use('books');
+	var nano = require('nano')('http://localhost:5984')
+	nano.db.create('books')
+	var books = nano.db.use('books')
 
 	//Insert a book document in the books database
 	books.insert({name: 'The Art of war'}, null, function(err, body) {
 	  if (!err){
-	    response.send(body);
+	    response.send(body)
 	  }
 	});
 
 	//Get a list of all books
 	books.list(function(err, body){
-	  response.send(body.rows);
-	}
+	  response.send(body.rows)
+	})
 
-
-  	response.send('Hello World!');
 })
 
 app.listen(app.get('port'), function() {
